0b6abffad5b68ecb562b67ef8ca615bc3ee9069d
Lukintosh <rockgsamesp@gmail.com>
2026-04-08 15:51:09 +0200

    Refactor server.js to use environment variables
    Removed unnecessary imports and added environment variables for client credentials.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const axios = require("axios");
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 const app = express();
 
