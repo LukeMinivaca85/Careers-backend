e13ace46d7d58834df8972090157c274ab2d77d6
Lukintosh <rockgsamesp@gmail.com>
2026-04-08 15:39:30 +0200

    Refactor LinkedIn auth endpoint with template literals

--- server.js
+++ server.js
@@ -12,14 +12,10 @@ app.get('/', (req, res) => {
   res.send("API Lukintosh rodando 🚀");
 });
 
-// LOGIN
-app.get('/auth/linkedin', (req, res) => {
-  const url =
-    "https://www.linkedin.com/oauth/v2/authorization" +
-    "?response_type=code" +
-    "&client_id=" + CLIENT_ID +
-    "&redirect_uri=" + encodeURIComponent(REDIRECT_URI) +
-    "&scope=r_liteprofile%20r_emailaddress";
+// app.get("/auth/linkedin", (req, res) => {
+  const redirect_uri = "https://careers-backend-dhpk.onrender.com/auth/linkedin/callback";
+
+  const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=r_liteprofile%20r_emailaddress`;
 
   res.redirect(url);
 });
